09fdcf8975d24f2a43dc5cb96a60a8ddc7c55f30
DevScriptTi <00.dev.00.dev.00@gmail.com>
2025-01-09 16:37:47 +0100

    NabBar has been modified (i have i add the active link propertie) for and i add a Dockerfile

--- components/Navbars/NavBar.jsx
+++ components/Navbars/NavBar.jsx
@@ -1,42 +1,62 @@
 "use client";
-import React from 'react';
-import Link from 'next/link';
-import SearchInput from './SearchInput';
+import React from "react";
+import Link from "next/link";
+import SearchInput from "./SearchInput";
+import { usePathname } from "next/navigation";
 
 const links = [
-    { href: '/', name: 'Home' },
-    { href: '/dashboard', name: 'Dashboard' },
-    { href: '/services', name: 'Services' },
-    { href: '/about', name: 'About' },
-    { href: '/contact', name: 'Contact' },
-    { href: '/login', name: 'Sign In' },
-    { href: '/register', name: 'Sign Out' }
+  { href: "/", name: "Home" },
+  { href: "/dashboard", name: "Dashboard" },
+  { href: "/services", name: "Services" },
+  { href: "/about", name: "About" },
+  { href: "/contact", name: "Contact" },
+  { href: "/login", name: "Sign In" },
+  { href: "/register", name: "Sign Out" },
 ];
 
 const NavBar = () => {
-    return (
-        <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="flex space-x-4">
-                    {links.slice(0, 5).map((link, index) => (
-                        <Link key={index} href={link.href} passHref>
-                            <span className="text-white cursor-pointer">{link.name}</span>
-                        </Link>
-                    ))}
-                </div>
-                <div className="flex-grow mx-4">
-                    <SearchInput />
-                </div>
-                <div className="flex space-x-4">
-                    {links.slice(5).map((link, index) => (
-                        <Link key={index} href={link.href} passHref>
-                            <span className="text-white cursor-pointer">{link.name}</span>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </nav>
-    );
+  const pahtname = usePathname();
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex space-x-4">
+          {links.slice(0, 5).map((link, index) => {
+            const isActive = pahtname === link.href;
+            return (
+              <Link key={index} href={link.href} passHref>
+                <span
+                  className={`text-white cursor-pointer ${
+                    isActive && "font-bold text-violet-700"
+                  }`}
+                >
+                  {link.name}
+                </span>
+              </Link>
+            );
+          })}
+        </div>
+        <div className="flex-grow mx-4">
+          <SearchInput />
+        </div>
+        <div className="flex space-x-4">
+          {links.slice(5).map((link, index) => {
+            const isActive = pahtname === link.href;
+            return (
+              <Link key={index} href={link.href} passHref>
+                <span
+                  className={`text-white cursor-pointer ${
+                    isActive && "font-bold text-violet-700"
+                  }`}
+                >
+                  {link.name}
+                </span>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
